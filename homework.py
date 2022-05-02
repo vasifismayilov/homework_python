1cca35edf0ca2f5f56a9a0398b493147e2599854
vasifismayilov <ismayilov2265@gmail.com>
2022-05-02 23:07:44 +0400

    homework_python

---
+++ homework.py
@@ -0,0 +1,60 @@
+############ 1
+#ferma = ['at', 'qoyun', 'inek', 'at', 'inek', 'qoyun', 'at', 'at', 'keci']
+
+#print(ferma.index('keci'))
+         #a
+
+#ferma.sort()
+#print(ferma)
+         #b
+
+#ferma.pop(0)
+#ferma.append('toyuq')
+#print(ferma)
+         #c
+
+#ferma.insert(0, 'keci')
+#print(ferma)
+        #d
+
+#half_ferma = round(len(ferma)/2)
+#print(ferma[0: half_ferma])
+        #e
+
+#new_animals = ['at', 'qoyun', 'inek', 'inek', 'qoyun']
+#big_ferm = ferma + new_animals
+#print(big_ferm)
+         #f
+
+#print(ferma*3)
+         #g
+         
+#ferma.reverse()         
+#print(ferma)
+          #h         
+                 
+#new_ferm = ferma.copy()
+#print(new_ferm)
+          #j
+
+#ferma.clear()
+#print(ferma) 
+           #k
+                    
+############ 3
+
+#say = list(map(int,input("Bir eded daxil edin: ").split(' ')))
+#print(say)
+
+############  4
+
+#numbers = [10.2, -32, -5.45, 32, 7, 43, -24.06, -4, -6.33, 62]
+#print(max(numbers),min(numbers))
+
+############  5
+#r=[31, 38, 69, 83, 83, 56, 38, 41, 96, 48, 43, 60, 49, 47, 73, 60, 69, 96, 50, 74]
+#ortalama = sum(r)/len(r)
+#print(len(r),)
+#print(ortalama)
+
+
